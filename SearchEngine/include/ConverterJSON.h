e1d6802ac762b75560b03df7b6cdf3481ba02181
Sanya2509kor <korchemkinaleksandr@yandex.ru>
2025-03-14 16:31:53 +0700

    ереименовал папку

---
+++ SearchEngine/include/ConverterJSON.h
@@ -0,0 +1,29 @@
+//
+// Created by user on 18.02.2025.
+//
+
+#ifndef CONVERTERJSON_H
+#define CONVERTERJSON_H
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "SearchServer.h"
+#include "../nlohmann_json/include/nlohmann/json.hpp"
+
+class ConverterJSON {
+public:
+    ConverterJSON();
+    std::vector<std::string> GetTextDocuments();
+    int GetResponsesLimit();
+    std::vector<std::string> GetRequests();
+    void putAnswers(std::vector<std::vector<RelativeIndex>>answers);
+private:
+    std::string config_json = "../config.json";
+    std::string requests_json = "../requests.json";
+    std::string answers_json = "../answers.json";
+};
+
+
+
+#endif //CONVERTERJSON_H
